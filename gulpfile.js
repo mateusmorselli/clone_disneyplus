3bd77ac337abc59858c486521d19dd2aa6be2a78
Mateus Morselli <matmorselli@gmail.com>
2025-02-24 18:16:26 -0300

    configurando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 
 //transforma os arquivos sass em css na pasta dist/css
@@ -16,9 +17,16 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
-exports.default = gulp.parallel(styles, images);
+
+exports.default = gulp.parallel(styles, images, scripts);
 //watch faz com que o servidor detecte alterações no codigo e já atualize sem precisar ficar digitando gulp toda vez
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
